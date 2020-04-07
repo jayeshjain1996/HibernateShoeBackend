55c8d689a73bfb0135ff5610729902f999064435
HP <HP@DESKTOP-ABPPQUS>
2020-04-07 13:07:22 +0530

    Changes

--- src/main/java/com/niit/model/Shoe.java
+++ src/main/java/com/niit/model/Shoe.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+//this is the main model
 @Entity
 @Table(name = "shoes")
 public class Shoe
